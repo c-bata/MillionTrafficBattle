fadd890d65cd21d2fe4e57f0ab3ad66977a2c64e
c-bata <contact@c-bata.link>
2015-10-17 17:14:12 +0900

    Implement read master data without tags

--- flask_api/api.py
+++ flask_api/api.py
@@ -2,7 +2,7 @@ from flask import jsonify, current_app
 from flask.ext.sqlalchemy import get_debug_queries
 
 from . import app, db
-from .models import User
+from .models import User, Item, Order, Tag
 
 
 @app.after_request
@@ -24,17 +24,41 @@ def get_order():
 @app.route('/initialize')
 def initialize():
     import csv
-    f = open(current_app.config['DATA_DIR'] + 'user.tsv')
-    tsv = csv.reader(f, delimiter='\t')
 
     db.drop_all()
     db.create_all()
 
+    # user.tsv
+    f = open(current_app.config['DATA_DIR'] + 'user.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+
     for i, row in enumerate(tsv):
-        if (i == 0):
+        if i == 0:
             continue
         user = User(row[0], row[1], row[2])
         db.session.add(user)
+    db.session.commit()
+
+    # item.tsv
+    f = open(current_app.config['DATA_DIR'] + 'item.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+
+    for i, row in enumerate(tsv):
+        if i == 0:
+            continue
+        item = Item(row[0], row[1], row[2], row[3])
+        db.session.add(item)
+    db.session.commit()
+
+    # order.tsv
+    f = open(current_app.config['DATA_DIR'] + 'order.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+
+    for i, row in enumerate(tsv):
+        if i == 0:
+            continue
+        order = Order(row[0], row[1], row[2], row[3], row[4], row[5])
+        db.session.add(order)
     db.session.commit()
 
     return 'Success'
