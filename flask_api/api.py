ba54206cffcce016e6ae8a67ec7b518878e1c5ef
c-bata <contact@c-bata.link>
2015-10-17 17:27:39 +0900

    Extract to command

--- flask_api/api.py
+++ flask_api/api.py
@@ -21,49 +21,6 @@ def get_order():
     return jsonify(Users=['wei', 'hai', 'dkjfk'])
 
 
-@app.route('/initialize')
-def initialize():
-    import csv
-
-    db.drop_all()
-    db.create_all()
-
-    # user.tsv
-    f = open(current_app.config['DATA_DIR'] + 'user.tsv')
-    tsv = csv.reader(f, delimiter='\t')
-
-    for i, row in enumerate(tsv):
-        if i == 0:
-            continue
-        user = User(row[0], row[1], row[2])
-        db.session.add(user)
-    db.session.commit()
-
-    # item.tsv
-    f = open(current_app.config['DATA_DIR'] + 'item.tsv')
-    tsv = csv.reader(f, delimiter='\t')
-
-    for i, row in enumerate(tsv):
-        if i == 0:
-            continue
-        item = Item(row[0], row[1], row[2], row[3])
-        db.session.add(item)
-    db.session.commit()
-
-    # order.tsv
-    f = open(current_app.config['DATA_DIR'] + 'order.tsv')
-    tsv = csv.reader(f, delimiter='\t')
-
-    for i, row in enumerate(tsv):
-        if i == 0:
-            continue
-        order = Order(row[0], row[1], row[2], row[3], row[4], row[5])
-        db.session.add(order)
-    db.session.commit()
-
-    return 'Success'
-
-
 @app.route('/')
 def users():
     users = User.query.all()
--- manage.py
+++ manage.py
@@ -2,9 +2,50 @@ from flask_api import app
 from flask.ext.script import Manager
 from flask.ext.migrate import MigrateCommand
 
+from flask_api import app, db
+from flask_api.models import User, Item, Order, Tag
+
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def init():
+    import csv
+
+    db.drop_all()
+    db.create_all()
+
+    # user.tsv
+    f = open(app.config['DATA_DIR'] + 'user.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+    print(tsv.__next__())
+
+    for row in tsv:
+        user = User(row[0], row[1], row[2])
+        db.session.add(user)
+    db.session.commit()
+
+    # item.tsv
+    f = open(app.config['DATA_DIR'] + 'item.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+    print(tsv.__next__())
+
+    for row in tsv:
+        item = Item(row[0], row[1], row[2], row[3])
+        db.session.add(item)
+    db.session.commit()
+
+    # order.tsv
+    f = open(app.config['DATA_DIR'] + 'order.tsv')
+    tsv = csv.reader(f, delimiter='\t')
+    print(tsv.__next__())
+
+    for row in tsv:
+        order = Order(row[0], row[1], row[2], row[3], row[4], row[5])
+        db.session.add(order)
+    db.session.commit()
 
 
 @manager.command
