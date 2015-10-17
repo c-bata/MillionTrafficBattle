da6b6c155abc12147c6aea15faf914995aba77d6
c-bata <contact@c-bata.link>
2015-10-17 09:55:29 +0900

    Add werkzeug profiling command

--- manage.py
+++ manage.py
@@ -8,5 +8,12 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.command
+def profile(length=25):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length])
+    app.run()
+
 if __name__ == '__main__':
     manager.run()
