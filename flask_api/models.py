3b5a415e717bcc88ba8f01a296779b426afd8caf
c-bata <contact@c-bata.link>
2015-10-18 02:12:15 +0000

    Bug fix orderTags

--- flask_api/models.py
+++ flask_api/models.py
@@ -70,5 +70,5 @@ class Order(db.Model):
             'orderItemId': self.order_item_id,
             'orderQuantity': self.order_quantity,
             'orderState': self.order_state,
-            'tags': [tag for tag in self.tags.split(',')]
+            'orderTags': [tag for tag in self.tags.split(',')]
         }
