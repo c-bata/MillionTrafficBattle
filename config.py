18f166c8849e2e50ea9b819d1ea1c57e7c73d49a
c-bata <contact@c-bata.link>
2015-10-18 02:17:06 +0000

    Unix domain socket on mysql

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), 'sample_data/')
 # database
 DEBUG = True
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mtb1:mtbpass1@localhost/mtb'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mtb1:mtbpass1@localhost/mtb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mtb1:mtbpass1@localhost/mtb?unix_socket=/var/lib/mysql/mysql.sock'
 
 SLOW_DB_QUERY_TIME = 0.5
 
